bb79e789c844ea7495209144995e6f255fdbabb0
Nathalie Fredriksson <nfredriksson1@gmail.com>
2026-03-02 11:13:31 +0100

    modell för böckerna i google api

--- models/Book.js
+++ models/Book.js
@@ -1,5 +1,20 @@
 const mongoose = require("mongoose")
 
 const bookSchema = new mongoose.Schema({
-    
+    googleId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    title: String,
+    authors: [String],
+    thumbnail: String,
+    description: String,
+
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
 })
+
+module.exports = mongoose.model("Book", bookSchema)
