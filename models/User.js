be506458154139e8fb45386faab146aa079aa12c
Nathalie Fredriksson <nfredriksson1@gmail.com>
2026-03-02 11:04:49 +0100

    utökade usermodellen

--- models/User.js
+++ models/User.js
@@ -1,8 +1,25 @@
 const mongoose = require("mongoose")
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true},
-    password: { type: String, required: true}
+    username: { 
+        type: String, 
+        required: true, 
+        unique: true,
+        minlength: 3 
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: { 
+        type: String, 
+        required: true 
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
 })
 
 module.exports = mongoose.model("User", userSchema)
