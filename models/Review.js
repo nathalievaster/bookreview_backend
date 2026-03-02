fa9231a3eb3a9024049c5b71d3e8b9720329b27c
Nathalie Fredriksson <nfredriksson1@gmail.com>
2026-03-02 11:09:52 +0100

    modell för recensioner, ksk inte behövs för böcker?

---
+++ models/Review.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose")
+const { string } = require("yup")
+
+const reviewSchema = new mongoose.Schema({
+    bookId: {
+        type: String,
+        required: true
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    reviewText: {
+        type: String,
+        required: true,
+        minlength: 5
+    },
+    rating: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 5
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date
+    }
+})
+
+module.exports = mongoose.model("Review", reviewSchema)
