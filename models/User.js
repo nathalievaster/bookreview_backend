cfc7f6796354e45c4134b25a9ea9f1f79ece9e9e
Nathalie Fredriksson <nfredriksson1@gmail.com>
2026-03-03 13:59:45 +0100

    lade till role i användarmodellen

--- models/User.js
+++ models/User.js
@@ -1,24 +1,29 @@
 const mongoose = require("mongoose")
 
 const userSchema = new mongoose.Schema({
-    username: { 
-        type: String, 
-        required: true, 
+    username: {
+        type: String,
+        required: true,
         unique: true,
-        minlength: 3 
+        minlength: 3
     },
     email: {
         type: String,
         required: true,
         unique: true
     },
-    password: { 
-        type: String, 
-        required: true 
+    password: {
+        type: String,
+        required: true
     },
     createdAt: {
         type: Date,
         default: Date.now
+    },
+    role: {
+        type: String,
+        enum: ["user", "admin"],
+        default: "user"
     }
 })
 
